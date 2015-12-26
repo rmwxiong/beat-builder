0dc6e8f823f128ada904afbf64ca879f68835970
rmwxiong <hododoho@gmail.com>
2015-12-26 15:56:34 -0800

    Create generic webpack app scaffold

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  'extends': 'google',
+  'es6': true,
+  'env': {
+    browser: true,
+    node: true
+  },
+  'globals': {
+  },
+  'rules': {
+    'max-len': [1, 120, 4],
+    'quote-props': [0, 'as-needed'],
+    'no-else-return': 0,
+    'require-jsdoc': 0,
+    'curly': 0,
+    'new-cap': 0
+  }
+};
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: {
+    app: ['./src/main.js']
+  },
+  output: {
+    path: require('path').resolve('public'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.styl$/,
+      loader: 'style-loader!css-loader!stylus-loader'
+    }, {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader'
+    }]
+  },
+  plugins: []
+};
